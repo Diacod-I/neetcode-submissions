099d2537f1bef44acd2244e50347a469c26df3db
advithkrishnan.eth <advithsonu@gmail.com>
2026-06-08 13:02:42 +0530

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -1,9 +1,16 @@
-from heapq import nlargest
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if k == len(nums):
-            return nums
-        counts = Counter(nums)
-        return nlargest(k, counts.keys(), key=counts.get)
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
